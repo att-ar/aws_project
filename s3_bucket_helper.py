ecd1d48c906d84e4c42cfb4cf5ab9541f755c573
Attar Aziz <attar_a@outlook.com>
2023-02-14 20:20:59 -0500

    print buckets given name[, date or date interval]

--- s3_bucket_helper.py
+++ s3_bucket_helper.py
@@ -1,6 +1,7 @@
 #function to add random suffix to bucket names
 import boto3
 from uuid import uuid4
+import datetime
 def gen_bucket_name(bucket_name: str) -> str:
     '''
     Precondition: `bucket_name` contains S3 valid characters
@@ -43,3 +44,67 @@ def gen_bucket(bucket_name: str, s3_boto_connection, suffix = True):
     )
     print(f"Bucket: {bucket_name}\tRegion: {user_region}")
     return bucket_name, bucket_response
+
+def helper_date_comparison(bucket_creationdate: datetime.datetime, *args) -> bool:
+    '''helper for get_buckets_with_name_date, returns the date comparison result'''
+    timezone = bucket_creationdate.tzinfo
+    args = list(args)
+    for i in range(len(args)):
+        if not args[i].tzinfo:
+            args[i] = args[i].replace(tzinfo = timezone)
+    if len(args) == 2:
+        assert args[0] < args[1], "start date must be before end date in `use_date`."
+        return bucket_creationdate <= args[1] and bucket_creationdate >= args[0]
+    else:
+        if not args[0]: #default value is None in get_bucket_with_name_date
+            return True #so all dates are retrieved
+        else:
+            return bucket_creationdate.date() == args[0].date()
+            #will use the date portion for single comparison
+def get_buckets_with_name_date(prefix: str,
+    use_date: list[str|datetime.datetime|datetime.date]
+                | str|datetime.datetime|datetime.date = None) -> list[str]:
+    '''
+    Returns list of buckets who start with `prefix` and made on or in (list) `use_date` using boto3.
+
+    Parameters:
+    `prefix` str
+        the first len(prefix) characters in bucket name must be `prefix` to work.
+        If you want all buckets, to only filter with date, use `prefix` = ""
+    `use_date` list[str|datetime.datetime|datetime.date] or str|datetime.datetime|datetime.date
+        defaults to None if only the name is needed as a search.
+        Used to retrieve the buckets made on the date given.
+        if str, must be in the format "year-month-day-hour-minute-second".
+            only year, month, and day are required.
+        If you want to check an interval of dates, use a for loop with an f-string for `use_date`.
+
+    Doesn't throw an indexing error if the prefix is longer than the bucket name.
+    '''
+    assert isinstance(use_date, (str, list, datetime.date, datetime.datetime)), f"Invalid type(use_date) = {type(use_date)}"
+    # turning use_date into datetime.datetime
+    if isinstance(use_date, str):
+        try:
+            use_date = [int(i) for i in use_date.split("-")]
+            use_date = [datetime.datetime(*use_date)]
+        except Exception:
+            return f"Incorrect string input for `use_date` = {use_date}"
+    elif isinstance(use_date, datetime.date):
+        use_date = [datetime.datetime.combine(use_date, datetime.time(0))]
+
+    elif isinstance(use_date, list):
+        assert len(use_date) == 2, "Date interval can only have two dates."
+        for i,d in enumerate(use_date):
+            if isinstance(d,str):
+                try:
+                    d = [int(num) for num in d.split("-")]
+                    use_date[i] = datetime.datetime(*d)
+                except Exception:
+                    return f"Incorrect string input for `use_date[{i}]` = {d}"
+            elif isinstance(d, datetime.date):
+                use_date[i] = datetime.datetime.combine(d, datetime.time(0))
+    #use_date is all datetime.datetime now
+
+    return [bucket for bucket
+        in boto3.client("s3").list_buckets()["Buckets"]
+        if (bucket["Name"][: min(len(bucket["Name"]), len(prefix))] == prefix)
+            & (helper_date_comparison(bucket["CreationDate"], *use_date))]
